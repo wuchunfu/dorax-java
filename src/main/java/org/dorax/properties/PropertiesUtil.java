10d5f06258328e66bed4f56c1abb3865104942be
wuchunfu <319355703@qq.com>
2019-12-13 20:05:00 +0800

    add util class

--- src/main/java/org/dorax/properties/PropertiesUtil.java
+++ src/main/java/org/dorax/properties/PropertiesUtil.java
@@ -6,6 +6,7 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
+import java.util.NoSuchElementException;
 import java.util.Objects;
 import java.util.Properties;
 
@@ -155,6 +156,122 @@ public class PropertiesUtil {
             e.printStackTrace();
             return "";
         }
+    }
+
+    /**
+     * 取出Property，但以System的Property优先,取不到返回空字符串.
+     *
+     * @param propertiesName 配置文件名称
+     * @param key            key 值
+     * @return 返回 string 类型的值
+     */
+    private String getValue(String propertiesName, String key) {
+        String systemProperty = System.getProperty(key);
+        if (systemProperty != null) {
+            return systemProperty;
+        }
+        Properties properties = getProperties(propertiesName);
+        if (properties.containsKey(key)) {
+            return properties.getProperty(key);
+        }
+        return "";
+    }
+
+    /**
+     * 取出String类型的Property，但以System的Property优先.如果都为Null则返回Default值.
+     *
+     * @param propertiesName 配置文件名称
+     * @param key            key 值
+     * @param defaultValue   默认值
+     * @return 返回 string 类型的值
+     */
+    public String getProperty(String propertiesName, String key, String defaultValue) {
+        String value = getValue(propertiesName, key);
+        return value != null ? value : defaultValue;
+    }
+
+    /**
+     * 取出Integer类型的Property，但以System的Property优先.如果都为Null或内容错误则抛出异常.
+     *
+     * @param propertiesName 配置文件名称
+     * @param key            key 值
+     * @return 返回 int 类型的值
+     */
+    public Integer getInteger(String propertiesName, String key) {
+        String value = getValue(propertiesName, key);
+        if (value == null) {
+            throw new NoSuchElementException();
+        }
+        return Integer.valueOf(value);
+    }
+
+    /**
+     * 取出Integer类型的Property，但以System的Property优先.如果都为Null则返回Default值，如果内容错误则抛出异常
+     *
+     * @param propertiesName 配置文件名称
+     * @param key            key 值
+     * @param defaultValue   默认值
+     * @return 返回 int 类型的值
+     */
+    public Integer getInteger(String propertiesName, String key, Integer defaultValue) {
+        String value = getValue(propertiesName, key);
+        return value != null ? Integer.valueOf(value) : defaultValue;
+    }
+
+    /**
+     * 取出Double类型的Property，但以System的Property优先.如果都为Null或内容错误则抛出异常.
+     *
+     * @param propertiesName 配置文件名称
+     * @param key            key 值
+     * @return 返回 double 类型的值
+     */
+    public Double getDouble(String propertiesName, String key) {
+        String value = getValue(propertiesName, key);
+        if (value == null) {
+            throw new NoSuchElementException();
+        }
+        return Double.valueOf(value);
+    }
+
+    /**
+     * 取出Double类型的Property，但以System的Property优先.如果都为Null则返回Default值，如果内容错误则抛出异常
+     *
+     * @param propertiesName 配置文件名称
+     * @param key            key 值
+     * @param defaultValue   默认值
+     * @return 返回 double 类型的值
+     */
+    public Double getDouble(String propertiesName, String key, Integer defaultValue) {
+        String value = getValue(propertiesName, key);
+        return value != null ? Double.parseDouble(value) : defaultValue;
+    }
+
+    /**
+     * 取出Boolean类型的Property，但以System的Property优先.如果都为Null抛出异常,如果内容不是true/false则返回false.
+     *
+     * @param propertiesName 配置文件名称
+     * @param key            key 值
+     * @return 如果内容为真则返回 true，否则返回 false
+     */
+    public Boolean getBoolean(String propertiesName, String key) {
+        String value = getValue(propertiesName, key);
+        if (value == null) {
+            throw new NoSuchElementException();
+        }
+        return Boolean.valueOf(value);
+    }
+
+    /**
+     * 取出Boolean类型的Property，但以System的Property优先.如果都为Null则返回Default值,如果内容不为true/false则返回false.
+     *
+     * @param propertiesName 配置文件名称
+     * @param key            key 值
+     * @param defaultValue   默认值
+     * @return 如果内容为真则返回 true，否则返回 false
+     */
+    public Boolean getBoolean(String propertiesName, String key, boolean defaultValue) {
+        String value = getValue(propertiesName, key);
+        return value != null ? Boolean.parseBoolean(value) : defaultValue;
     }
 
     public static void main(String[] args) {
