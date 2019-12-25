4bae754a319e07e7574cd35c74be0bb4c54d8e26
wuchunfu <319355703@qq.com>
2019-12-25 23:35:46 +0800

    add util class

--- src/main/java/org/dorax/lang/NumberUtils.java
+++ src/main/java/org/dorax/lang/NumberUtils.java
@@ -2,7 +2,9 @@ package org.dorax.lang;
 
 import java.math.BigDecimal;
 import java.math.BigInteger;
+import java.math.RoundingMode;
 import java.text.DecimalFormat;
+import java.text.NumberFormat;
 
 /**
  * BigDecimal工具类
@@ -136,5 +138,95 @@ public class NumberUtils {
             return ((Long) object);
         }
         return null;
+    }
+
+    /**
+     * 保留指定位数的小数(少的位数不补零)
+     *
+     * @param value 双精度值
+     * @param n     小数点位数
+     * @return 保留指定位数的小数的值
+     */
+    public static String keepRandomPoint(Double value, int n) {
+        if (value == null) {
+            value = 0.00;
+        }
+        return new BigDecimal(value).setScale(n, RoundingMode.HALF_UP).toString();
+    }
+
+    /**
+     * 浮点保留两位小数(少的位数不补零)
+     *
+     * @param value 双精度值
+     * @return 保留两位小数的值
+     */
+    public static String keep2Point(double value) {
+        return keepRandomPoint(value, 2);
+    }
+
+    /**
+     * 浮点保留1位小数(少的位数不补零)
+     *
+     * @param value 双精度值
+     * @return 保留一位小数的值
+     */
+    public static String keep1Point(double value) {
+        return keepRandomPoint(value, 1);
+    }
+
+    /**
+     * 浮点保留任意位小数(少位补零)
+     *
+     * @param value 双精度值
+     * @param n     小数点位数
+     * @return 保留任意位小数的值
+     */
+    public static String keepRandomPointZero(double value, int n) {
+        DecimalFormat df = new DecimalFormat("#0.00");
+        return df.format(Double.valueOf(keepRandomPoint(value, n)));
+    }
+
+    /**
+     * 浮点保留两位小数(少位补零)
+     *
+     * @param value 双精度值
+     * @return 保留两位小数的值
+     */
+    public static String keep2PointZero(double value) {
+        return keepRandomPointZero(value, 2);
+    }
+
+    /**
+     * 获取任意小数点位的百分比表示
+     *
+     * @param value 双精度值
+     * @param n     小数点位数
+     * @return 获取任意小数点位的百分比表示的值
+     */
+    public static String percentRandomPoint(double value, int n) {
+        NumberFormat percent = NumberFormat.getPercentInstance();
+        percent.setGroupingUsed(false);
+        percent.setMaximumFractionDigits(n);
+        return percent.format(value);
+    }
+
+    /**
+     * 百分比保留两位小数
+     *
+     * @param value 双精度值
+     * @return 百分比保留两位小数的值
+     */
+    public static String percent2Point(double value) {
+        return percentRandomPoint(value, 2);
+    }
+
+    /**
+     * 获取格式化经纬度后的小数(保留3位)
+     *
+     * @param value 双精度值
+     * @return 获取格式化经纬度后的小数(保留3位)的值
+     */
+    public static String latLngPoint(double value) {
+        return keepRandomPoint(value, 3);
     }
 }
