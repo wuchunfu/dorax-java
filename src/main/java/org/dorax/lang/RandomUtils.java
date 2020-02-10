1da16b667bc962688559f7eafc68c8dd9540ec2f
wuchunfu <319355703@qq.com>
2020-02-10 15:24:04 +0800

    add util class

--- src/main/java/org/dorax/lang/RandomUtils.java
+++ src/main/java/org/dorax/lang/RandomUtils.java
@@ -3,12 +3,81 @@ package org.dorax.lang;
 import java.util.Random;
 
 /**
- * Random 工具类
+ * 随机数工具类
  *
  * @author wuchunfu
- * @date 2020-01-11
+ * @date 2020-02-05
  */
 public class RandomUtils {
+
+    private static final String ALL_CHAR = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    private static final String LETTER_CHAR = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    private static final String NUMBER_CHAR = "0123456789";
+
+    /**
+     * 获取定长的随机数，包含大小写、数字
+     *
+     * @param length 随机数长度
+     * @return 随机字符串
+     */
+    public static String generateString(int length) {
+        StringBuilder sb = new StringBuilder();
+        Random random = new Random();
+        for (int i = 0; i < length; i++) {
+            sb.append(ALL_CHAR.charAt(random.nextInt(ALL_CHAR.length())));
+        }
+        return sb.toString();
+    }
+
+    /**
+     * 获取定长的随机数，包含大小写字母
+     *
+     * @param length 随机数长度
+     * @return 随机字符串
+     */
+    public static String generateMixString(int length) {
+        StringBuilder sb = new StringBuilder();
+        Random random = new Random();
+        for (int i = 0; i < length; i++) {
+            sb.append(LETTER_CHAR.charAt(random.nextInt(LETTER_CHAR.length())));
+        }
+        return sb.toString();
+    }
+
+    /**
+     * 获取定长的随机数，只包含小写字母
+     *
+     * @param length 随机数长度
+     * @return 随机字符串
+     */
+    public static String generateLowerString(int length) {
+        return generateMixString(length).toLowerCase();
+    }
+
+    /**
+     * 获取定长的随机数，只包含大写字母
+     *
+     * @param length 随机数长度
+     * @return 随机字符串
+     */
+    public static String generateUpperString(int length) {
+        return generateMixString(length).toUpperCase();
+    }
+
+    /**
+     * 获取定长的随机数，只包含数字
+     *
+     * @param length 随机数长度
+     * @return 随机字符串
+     */
+    public static String generateNumberString(int length) {
+        StringBuilder sb = new StringBuilder();
+        Random random = new Random();
+        for (int i = 0; i < length; i++) {
+            sb.append(NUMBER_CHAR.charAt(random.nextInt(NUMBER_CHAR.length())));
+        }
+        return sb.toString();
+    }
 
     protected static final Random RANDOM = new Random();
 
