d6ab9a20096b5ec6637a5394104a1fa89ce8df5d
wuchunfu <319355703@qq.com>
2020-02-23 23:41:14 +0800

    add util class

--- src/main/java/org/dorax/codec/Md5Utils.java
+++ src/main/java/org/dorax/codec/Md5Utils.java
@@ -1,5 +1,6 @@
 package org.dorax.codec;
 
+import java.io.UnsupportedEncodingException;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 
@@ -50,6 +51,50 @@ public class Md5Utils {
             buffer.append(Long.toString((int) aByte & 0xff, 16));
         }
         return buffer.toString();
+    }
+
+    /**
+     * 获取加密字符串
+     *
+     * @param value  明文字符串
+     * @param encode 字符编码
+     * @return 秘闻字符串
+     */
+    public static String getMd5(String value, String encode) {
+        String result = "";
+        try {
+            result = getMd5(value.getBytes(encode));
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+        }
+        return result;
+    }
+
+    private static final int HEX_VALUE_COUNT = 16;
+
+    /**
+     * 获取加密字符串
+     *
+     * @param bytes 明文字节
+     * @return 秘闻字符串
+     */
+    public static String getMd5(byte[] bytes) {
+        char[] hexDigits = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f'};
+        char[] str = new char[16 * 2];
+        try {
+            MessageDigest md = MessageDigest.getInstance("MD5");
+            md.update(bytes);
+            byte[] tmp = md.digest();
+            int k = 0;
+            for (int i = 0; i < HEX_VALUE_COUNT; i++) {
+                byte byte0 = tmp[i];
+                str[k++] = hexDigits[byte0 >>> 4 & 0xf];
+                str[k++] = hexDigits[byte0 & 0xf];
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return new String(str);
     }
 
     public static void main(String[] args) {
